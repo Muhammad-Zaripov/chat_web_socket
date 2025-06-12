0c59460819aaecad346d4dcc1ea10b0957ffdc75
Muhammad Zaripov <muhammadzaripov@MacBook-Pro-Muhammad.local>
2025-06-12 08:47:02 +0500

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:chat_web_socket/feature/chat/presentation/bloc/chat_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'feature/chat/presentation/screens/chat_screen.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+  await Hive.openBox('chatBox');
   runApp(const MainApp());
 }
 
